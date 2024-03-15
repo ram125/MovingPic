4451ad511ed9f386aa51c0489ae9b6fb7a666484
r.dain24 <r.dain24@LAB108PC12>
2024-03-15 12:38:56 +0200

    Add project files.

---
+++ MovingPic/Form1.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MovingPic
+{
+    public partial class Form1 : Form
+    {
+        public int x;
+        public int y;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            int x = pictureBox1.Location.X;
+            int y = pictureBox1.Location.Y;
+            timer1.Start();
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            if (x > 500)
+            {
+                x = -410;
+            }
+            x = x+5;
+            pictureBox1.Location = new Point(x, y);
+        }
+    }
+}
